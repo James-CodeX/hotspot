7d25e76230d14ec6fdc048066b26b90054bc936f
James Karanja <124869546+James-CodeX@users.noreply.github.com>
2026-03-10 15:59:54 +0300

    feat: configure dynamic Apache port handling in start-apache.sh

--- docker/start-apache.sh
+++ docker/start-apache.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+PORT="${PORT:-80}"
+
 # Railway images occasionally surface duplicate MPM enables.
 # Force Apache back to a single prefork MPM before startup.
 rm -f /etc/apache2/mods-enabled/mpm_event.load \
@@ -15,5 +17,9 @@ fi
 if [ ! -e /etc/apache2/mods-enabled/mpm_prefork.conf ]; then
     ln -s ../mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
 fi
+
+sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+export APACHE_LISTEN_PORT="$PORT"
 
 exec apache2-foreground
